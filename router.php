0dfaebc5955da7f6e2559dcc0fb1a1cc2705b924
Anton <135710412+f4soul@users.noreply.github.com>
2023-07-11 11:54:12 +0300

    Add files via upload

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+$url = explode("/", $_SERVER['REQUEST_URI']);
+require_once("php/db.php");
+require_once("php/classes/User.php");
+
+
+if ($url[1] == "blog") {
+	$content = file_get_contents("pages/blog.php");
+} else if ($url[1] == "auth") {
+	$content = file_get_contents("pages/login.html");
+} else if ($url[1] == "register") {
+	$content = file_get_contents("pages/register.html");
+} else if ($url[1] == "users") {
+	require_once("pages/users/index.html");
+} else if ($url[1] == "addUser") {
+	echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else {
+	$content = file_get_contents("pages/index.php");
+}
+
+if (!empty($content)) require_once("template.php");
